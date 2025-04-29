77a23a7f202f00e5ebb0e7db5df578cd8f50d307
v-tnta <vox.kkn12@gmail.com>
2025-04-29 15:04:31 +0900

    v0.0.1 (Mock)

--- CSA.py
+++ CSA.py
@@ -1,6 +1,7 @@
 import flet as ft
 import asyncio
 from google import genai
+from google.genai import types
 
 def main(page: ft.Page):
     # アプリのタイトルを設定
@@ -25,29 +26,46 @@ def main(page: ft.Page):
         read_only=True,
         value="N/A",
         multiline=True,
-        min_lines=4
+        min_lines=1,
+        max_lines=10
     )
 
     def ask_gemini(input_text):
         # ここにGemini APIを呼び出すコードを追加
         # 例: response = await call_gemini_api(prompt)
-        print('status 0')
+
+        print('status 0') # << - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - debug
+
         with open('apikey.txt', 'r') as f: 
             apiKEY = f.read()
+        with open('prompt.txt', 'r') as f: 
+            prompt = f.read()
+        with open('userinfo.txt', 'r') as f: 
+            userinfo = f.read()
+
         client = genai.Client(api_key=apiKEY)
-        print('status 1')
+
+        print('status 1')  # << - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - debug
 
         try:
-            print('status 2')
+            print('status 2')  # << - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - debug
         
-            response = client.models.generate_content(model="gemini-2.0-flash", contents = [input_text]) # get API result
+            response = client.models.generate_content(
+                model="gemini-2.0-flash",
+                contents = [
+                    #input_text
+                    f"「ユーザーが提示したアイテム」: {input_text}"
+                    ],
+                config = types.GenerateContentConfig(
+                        system_instruction=f"{prompt}\n尚、「ユーザー情報」は以下の通り\n{userinfo}", # プロンプト @ here
+                ),
+            ) # get API result
         
-            print('status 3')
+            print('status 3')  # << - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - debug
             res = response.text
             
         except Exception as e:
             res = f"ERROR: {e}"
-
 
         return res
     
@@ -67,7 +85,19 @@ def main(page: ft.Page):
         icon=ft.icons.SEND
     )
     
-    # クリアボタンを作成
+    
+    # 画像選択ボタン
+    def pick_files_result(e: ft.FilePickerResultEvent):
+        # ongoing
+        pass
+    
+    image_button = ft.OutlinedButton(
+        "画像選択",
+        on_click=pick_files_result,
+        icon=ft.icons.IMAGE
+    )
+
+    # クリアボタン
     def clear_fields(e):
         input_field.value = ""
         output_field.value = "N/A"
@@ -84,7 +114,7 @@ def main(page: ft.Page):
         ft.Column(
             [
                 ft.Text("Clothing Suggestion App", size=30, weight=ft.FontWeight.BOLD),
-                ft.Text("version  0.1.0", size=16),
+                ft.Text("version  0.1.1", size=16),
                 ft.Divider(), # ----------------
                 
                 ft.Container(height=20),
@@ -95,7 +125,7 @@ def main(page: ft.Page):
                 ft.Container(height=20),
                 
                 ft.Row(
-                    [transfer_button, clear_button],
+                    [transfer_button, image_button, clear_button],
                     alignment=ft.MainAxisAlignment.CENTER
                 ),
                 
