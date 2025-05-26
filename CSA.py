129b3033fe6c06114c666c10ffcb9c9b0d49397f
v-tnta <vox.kkn12@gmail.com>
2025-05-26 14:17:20 +0900

    version v0.1.3 画像から服の情報を説明するセクション追加 promptC→for Coorfinate promptD → for Descripition

--- CSA.py
+++ CSA.py
@@ -1,17 +1,35 @@
-import flet as ft
+import flet as ft # pip install flet
 from google import genai
-from google.genai import types
-#from settings import show_settings_page
+from google.genai import types # pip install google-genai
+from PIL import Image # pip install Pillow
+
 
 def main(page: ft.Page):
-    page.title = "Clothing Suggestion App"
+    page.title = "Coordinate Suggestion App"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.padding = 20
 
+    #FilePicker
+    def pick_file_result(e: ft.FilePickerResultEvent):
+        selected_files.value = e.files[0].path if e.files else ""
+        selected_files.update()
+    
+    file_picker = ft.FilePicker(
+        on_result=pick_file_result
+    )
+
+    selected_files = ft.Text("picked files here") # 選択されたファイル名を表示するテキスト
+    pic_text = ft.Text("N/A") # 画像の説明を表示するテキスト
+
+
     #送信ボタン
     send_button = ft.ElevatedButton(
-        "送信", icon=ft.icons.SEND
+        "送信", icon=ft.Icons.SEND
+    )
+
+    send_button2 = ft.ElevatedButton(
+        "Ask Gemini", icon=ft.Icons.SEND
     )
 
     # 入力フィールド
@@ -20,7 +38,7 @@ def main(page: ft.Page):
         width=400,
         hint_text="input here",
         multiline=True,
-        min_lines=2,
+        min_lines=2
     )
 
     # Markdown出力欄（中身は後で更新）
@@ -28,25 +46,60 @@ def main(page: ft.Page):
         value="N/A",
         selectable=True,
         extension_set=ft.MarkdownExtensionSet.GITHUB_WEB,
-        on_tap_link=lambda e: page.launch_url(e.data),
+        on_tap_link=lambda e: page.launch_url(e.data)
     )
 
     # Markdownを囲む青い枠＋スクロール対応
     output_box = ft.Container(
         content=ft.Column([output_markdown], scroll=ft.ScrollMode.ALWAYS),
         padding=10,
-        border=ft.border.all(2, ft.colors.INDIGO),
+        border=ft.border.all(2, ft.Colors.INDIGO),
         border_radius=8,
-        width=600,
-        height=400,  # 固定高さ（超えたらスクロール）
+        width=500,
+        height=300,  # 固定高さ（超えたらスクロール）
     )
 
+    # ボタンをローディング表示に切り替え
+    def change_button(onchange, instruction_type):
+        if instruction_type == 0:
+
+            if (onchange==1):
+                send_button.disabled = True
+                send_button.icon = None
+                send_button.text = ""
+                send_button.content = ft.Row(
+                    [ft.ProgressRing(width=16, height=16)],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                )
+            else:          
+                send_button.content = None
+                send_button.icon =ft.Icons.SEND
+                send_button.text = "送信"
+                send_button.disabled = False
+
+        elif   instruction_type == 1:
+            if (onchange==1):
+                send_button2.disabled = True
+                send_button2.icon = None
+                send_button2.text = ""
+                send_button2.content = ft.Row(
+                    [ft.ProgressRing(width=16, height=16)],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                )
+            else:          
+                send_button2.content = None
+                send_button2.icon =ft.Icons.SEND
+                send_button2.text = "Ask Gemini"
+                send_button2.disabled = False
+
+        page.update()
+
     # Gemini呼び出し関数
-    def ask_gemini(input_text):
+    def ask_gemini_coordinate(input_text):
         try:
             with open("apikey.txt", "r", encoding='utf-8') as f:
                 apiKEY = f.read()
-            with open("prompt.txt", "r", encoding='utf-8') as f:
+            with open("promptC.txt", "r", encoding='utf-8') as f:
                 prompt = f.read()
             with open("userinfo.txt", "r", encoding='utf-8') as f:
                 userinfo = f.read()
@@ -64,32 +117,37 @@ def main(page: ft.Page):
         except Exception as e:
             return f"ERROR: {e}"
 
-    def change_button(onchange):
-        if (onchange==1):
-            # ボタンをローディング表示に切り替え
-            send_button.disabled = True
-            send_button.icon = None
-            send_button.text = ""
-            send_button.content = ft.Row(
-                [ft.ProgressRing(width=16, height=16)],
-                alignment=ft.MainAxisAlignment.CENTER,
-            )
-        else:          
-            send_button.content = None
-            send_button.icon =ft.icons.SEND
-            send_button.text = "送信"
-            send_button.disabled = False
+    def ask_gemini_description():
+        try:
+            with open("apikey.txt", "r", encoding='utf-8') as f:
+                apiKEY = f.read()
+            with open("promptD.txt", "r", encoding='utf-8') as f:
+                prompt = f.read()
 
-        
-        page.update()
-            
+            client = genai.Client(api_key=apiKEY)
+
+            image = Image.open(selected_files.value)
+            response = client.models.generate_content(
+                model="gemini-2.0-flash",
+                contents=[image, prompt]
+            )
+            return response.text
+        except Exception as e:
+            return f"ERROR: {e}"
+
 
     # 送信処理
-    def interface_gemini(e):
-        change_button(onchange=1)
-        response = ask_gemini(input_field.value)
-        change_button(onchange=0)
-        output_markdown.value = response
+    def interface_gemini(instruction_type):
+        if instruction_type == 0:
+            change_button(onchange=1, instruction_type=0)
+            response = ask_gemini_coordinate(input_field.value)
+            change_button(onchange=0, instruction_type=0)
+            output_markdown.value = response
+        elif instruction_type == 1:
+            change_button(onchange=1, instruction_type=1)
+            response = ask_gemini_description()
+            change_button(onchange=0, instruction_type=1)
+            pic_text.value = response
         page.update()
 
     # クリア処理
@@ -100,7 +158,8 @@ def main(page: ft.Page):
 
 
     #関数登録
-    send_button.on_click = lambda e: interface_gemini(e)
+    send_button.on_click = lambda e: interface_gemini(0)
+    send_button2.on_click = lambda e: interface_gemini(1)
 
     # HOME page
     def show_route_page():
@@ -108,13 +167,16 @@ def main(page: ft.Page):
             "/home",
             [
                 ft.AppBar(
-                    ft.IconButton(icon=ft.icons.SETTINGS, on_click=lambda e:page.go("/settings"))
-                ),
+                    ft.Row([
+                        ft.IconButton(icon=ft.Icons.SETTINGS, on_click=lambda e: page.go("/settings"))
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER)
+                    ),
 
                 ft.Column(
                     [
-                        ft.Text("Clothing Suggestion App", size=30, weight=ft.FontWeight.BOLD),
-                        ft.Text("version 0.1.2 | powered by gemini-2.0-flash", size=16),
+                        ft.Text("Coordinate Suggestion App", size=30, weight=ft.FontWeight.BOLD),
+                        ft.Text("version 0.1.3 | powered by gemini-2.0-flash", size=16),
                         ft.Divider(),
                         ft.Container(height=20),
                         ft.Text("入力:", size=14),
@@ -123,7 +185,8 @@ def main(page: ft.Page):
                         ft.Row(
                             [
                                 send_button,
-                                ft.OutlinedButton("クリア", on_click=clear_fields, icon=ft.icons.CLEAR),
+                                ft.OutlinedButton("クリア", on_click=clear_fields, icon=ft.Icons.CLEAR),
+                                ft.OutlinedButton("アイテム登録", on_click=lambda e: page.go("/additems"), icon=ft.Icons.ADD)
                             ],
                             alignment=ft.MainAxisAlignment.CENTER,
                         ),
@@ -159,12 +222,47 @@ def main(page: ft.Page):
             ]
         )
     
+
+    def show_additems_page():
+        page.overlay.append(file_picker)
+        page.update() # page.overlayへの追加を反映させる
+
+        return ft.View(
+            "/additems",
+            [
+                ft.Column(
+                    [
+                        ft.Divider(),
+                        ft.Text("This is the page to adding items"),
+                        # fileupload button
+                        ft.ElevatedButton(
+                            "pick a picture", 
+                            icon=ft.Icons.UPLOAD_FILE,
+                            on_click=lambda e :file_picker.pick_files(
+                                allow_multiple=False,   
+                                allowed_extensions=["jpg", "jpeg", "png", "bmp"])
+                        ),
+                        selected_files,
+                        send_button2,
+                        pic_text,
+                        ft.Divider(),
+                        ft.ElevatedButton("Go Back", on_click=lambda e: page.go("/home")),
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                    spacing=5,
+                )
+            ]
+        )
+    
     def route_change(hnd):
         page.views.clear()
         if hnd.route == "/home":
             page.views.append(show_route_page())
         elif hnd.route == "/settings":
             page.views.append(show_settings_page())
+        elif hnd.route == "/additems":
+            page.views.append(show_additems_page())       
         page.update()
 
     page.on_route_change = route_change
